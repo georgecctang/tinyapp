f6ed4b389c438d71c033488c8ec960e08f69352d
George Tang <georgecctang@gmail.com>
2020-09-28 20:57:36 +0000

    installed and required body-parser

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const urlencoded = require("body-parser/lib/types/urlencoded");
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.use(bodyParser, urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
